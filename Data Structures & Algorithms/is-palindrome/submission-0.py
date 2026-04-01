6c000ddc40117de572922d63b924c64f9a239080
Muhammad Raheel <115684857+raheelmuhd@users.noreply.github.com>
2026-04-01 08:04:23 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        for i in s:
+            if i == " ":
+                s = s.replace(" ", "")
+
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+
+            if not s[r].isalnum():
+                r-=1
+                continue
+            if not s[l].isalnum():
+                l+=1
+                continue
+
+
+
+            if s[l].lower() == s[r].lower():
+                l+=1
+                r-=1
+            else:
+                return False
+        
+        return True
+
+
+        
